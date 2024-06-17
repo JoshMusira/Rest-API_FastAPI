b6db53e2317199d55e93d66e4c3089f58d64a0c5
JoshMusira <joshuamusira01@gmail.com>
2024-06-17 23:42:52 +0300

    feat: add tags and models

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import Depends, FastAPI, HTTPException, status, Response
-from . import schemas, models
-from .database import SessionLocal, engine
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException, Request, status, Response
+from blog import schemas, models  # absolute imports
+from blog.database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -11,11 +13,10 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
-
     finally:
         db.close()
 
-@app.post('/create', status_code=status.HTTP_201_CREATED)
+@app.post('/create', status_code=status.HTTP_201_CREATED,  tags=['Blogs'])
 async def create_item(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -23,36 +24,53 @@ async def create_item(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog],  tags=['Blogs'])
 async def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog,  tags=['Blogs'])
 async def get_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {'error': f'Blog with id {id} not found'}
-    
     return blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,  tags=['Blogs'])
 async def delete_blog(id, response: Response, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog with id {id} not found'}
+    blog.delete(synchronize_session=False)
     db.commit()
-    return "Deleted successfull"
-    # if not blog:
-    #     response.status_code = status.HTTP_404_NOT_FOUND
-    #     return {'error': f'Blog with id {id} not found'}
-    
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+    return "Deleted successfully"
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED,  tags=['Blogs'])
 async def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, error=f"Blog with {id} not found.")
-    blog.update(request)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with {id} not found.")
+    blog.update(request.dict())
     db.commit()
-    
     return "Updated successfully"
+
+
+@app.post('/user', response_model=schemas.ShowUser,  tags=['Users'])
+async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=user.name, email=user.email, password=Hash.bcrypt(user.password))
+    # new_user = models.User(Request)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
     
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['Users'])  
+async def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, error=f"User  with {id} is not found")
+    return user
+
--- models.py
+++ models.py
@@ -8,3 +8,16 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creater = relationship("User", back_populates="blogs")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+    blogs = relationship("blogs", back_populates="creater")
